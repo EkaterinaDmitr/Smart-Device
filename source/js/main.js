32d8bc27c90b9dd080ee3cf7960d7512d9a7fcc0
EkaterinaDmitr <ketrins-lite_94@mail.ru>
2022-08-23 00:20:20 +0300

    исправление багов

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 
@@ -63,38 +64,81 @@ const initFooter = () => {
 };
 
 
-const formPhone = document.getElementById('formPhone');
+// const formPhone = document.getElementById('formPhone');
 
-formPhone.onclick = function () {
-  formPhone.value = '+7(';
-};
+// formPhone.onclick = function () {
+//   formPhone.value = '+7(';
+// };
 
-const old = 0;
+// const old = 0;
 
-formPhone.onkeydown = function () {
-  const curLen = formPhone.value.length;
+// formPhone.onkeydown = function () {
+//   const curLen = formPhone.value.length;
 
-  if (curLen < old) {
-    old--;
-    return;
-  }
-  if (curLen === 2)
-    formPhone.value = formPhone.value + '(';
+//   if (curLen < old) {
+//     old--;
+//     return;
+//   }
+//   if (curLen === 2)
+//     formPhone.value = formPhone.value + '(';
 
-  if (curLen === 6)
-    formPhone.value = formPhone.value + ')-';
+//   if (curLen === 6)
+//     formPhone.value = formPhone.value + ')-';
 
-  if (curLen == 11)
-    formPhone.value = formPhone.value + '-';
+//   if (curLen == 11)
+//     formPhone.value = formPhone.value + '-';
 
-  if (curLen == 14)
-    formPhone.value = formPhone.value + '-';
+//   if (curLen == 14)
+//     formPhone.value = formPhone.value + '-';
 
-  if (curLen > 16)
-    formPhone.value = formPhone.value.substring(0, formPhone.value.length - 1);
+//   if (curLen > 16)
+//     formPhone.value = formPhone.value.substring(0, formPhone.value.length - 1);
 
-  old++;
-};
+//   old++;
+// };
+
+[].forEach.call(
+    document.querySelectorAll('input[type="tel"]'),
+    function (input) {
+      let keyCode;
+      function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        let pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        let matrix = "+7 (___) ___ ____",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, ""),
+          new_value = matrix.replace(/[_\d]/g, function (a) {
+            return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+          });
+        i = new_value.indexOf('_');
+        if (i !== -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i);
+      }
+      let reg = matrix
+        .substr(0, this.value.length)
+        .replace(/_+/g, function (a) {
+            return '\\d{1,' + a.length + '}';
+          })
+          .replace(/[+()]/g, '\\$&');
+        reg = new RegExp('^' + reg + '$');
+        if (
+          !reg.test(this.value) ||
+        this.value.length < 5 ||
+        (keyCode > 47 && keyCode < 58)
+        )
+          this.value = new_value;
+        if (event.type === 'blur' && this.value.length < 5) this.value = "";
+      }
+
+      input.addEventListener('input', mask, false);
+      input.addEventListener('blur', mask, false);
+      input.addEventListener('keydown', mask, false);
+      input.addEventListener('focus', mask, false);
+    }
+);
 // ---------------------------------
 
 // ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
